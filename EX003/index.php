2d19851fcfa23163a6f8f524c28c7a1699814e71
lbeneylton <47756391+lbeneylton@users.noreply.github.com>
2025-03-19 22:41:05 -0300

    declarei variaveis e constantes

---
+++ EX003/index.php
@@ -0,0 +1,29 @@
+<?php 
+
+//Variáveis
+$nome = "Carlos";
+$idade = 22;
+$sobrenome = "Ferraz";
+
+//Constantes
+//Constantes não usam o $ para serem declaradas
+const PAIS = "Brasil";
+
+
+
+echo "Meu nome é $nome $sobrenome e tenho $idade anos";
+echo "<br>";
+
+
+//Constantes não usam o $ para serem chamadas
+
+
+echo "Moro no(a) ".PAIS;
+
+
+
+
+
+
+
+?>
